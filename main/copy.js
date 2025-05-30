9879acf2ab65a2e593b2580af58729aa65fe349e
dylnbk <dyln.bk@gmail.com>
2025-05-30 12:09:45 +0700

    add copy info for content

--- main/copy.js
+++ main/copy.js
@@ -1,5 +1,9 @@
 const shell = require('shelljs')
 
-// copy netlify functions and admin folder after build completes
+// copy netlify functions, admin folder, content folder, and assets after build completes
 shell.cp('-R', 'netlify', 'dist/')
 shell.cp('-R', 'admin', 'dist/')
+shell.cp('-R', 'content', 'dist/')
+shell.cp('-R', 'assets', 'dist/')
+
+console.log('Successfully copied netlify, admin, content, and assets folders to dist/')
