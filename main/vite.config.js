18eec9db1256d969c164dbc738ffd94182109a2c
dylnbk <dyln.bk@gmail.com>
2025-06-02 14:25:39 +0700

    updated CMS

--- main/vite.config.js
+++ main/vite.config.js
@@ -3,12 +3,14 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  publicDir: 'main/assets',
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         contact: resolve(__dirname, 'contact/contact.html')
       }
-    }
+    },
+    copyPublicDir: true
   }
 })
